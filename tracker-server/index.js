551700f85cbed61b728e2ed85ebdc1129d0b3696
Doğukan Süme <dogubey2112@gmail.com>
2026-03-27 01:17:52 +0300

    Initial backend setup with Express and MongoDB connection

---
+++ tracker-server/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+// Create an express application instance
+const app = express();
+
+// MongoDB connection string with credentials
+const mongoUri = 'mongodb+srv://admin:emir2121@cluster0.jjbixd4.mongodb.net/tracker';
+
+// Connect to the MongoDB database
+mongoose.connect(mongoUri);
+
+// Check if the database connection was successful
+mongoose.connection.on('connected', () => {
+  console.log('Connected to mongo instance');
+});
+
+// Log an error if the connection fails
+mongoose.connection.on('error', (err) => {
+  console.error('Error connecting to mongo', err);
+});
+
+// Main route that returns a simple message
+app.get('/', (req, res) => {
+  res.send('Hi there!');
+});
+
+// Start the server and listen on port 3000
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
