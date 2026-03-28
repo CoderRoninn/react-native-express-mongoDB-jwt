ecc436b01353d9b12f3bd92725f62fc4772103ec
Doğukan Süme <dogubey2112@gmail.com>
2026-03-28 17:56:03 +0300

    refactor: extract route paths to centralized constants

---
+++ tracker-server/src/constants/routes.js
@@ -0,0 +1,8 @@
+/**
+ * Route Constants
+ * Centralized route paths to avoid hardcoded strings
+ */
+
+export const ROUTES = Object.freeze({
+    SIGNUP: '/signup'
+});
--- tracker-server/src/routes/authRoutes.js
+++ tracker-server/src/routes/authRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
+import { ROUTES } from '../constants/routes.js';
 
 // Create a new router instance for grouping routes
 const router = express.Router();
 
 // Define a POST route for user signup
-router.post('/signup', (request, response) => {
+router.post(ROUTES.SIGNUP, (request, response) => {
     response.send('You made a post request');
 });
 
